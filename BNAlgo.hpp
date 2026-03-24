e13f562bde25c54827533cdb7ac19c1b8c08b1d7
Conner685 <cponton@my.bcit.ca>
2026-03-24 12:41:42 -0700

    imported openCV packages and created header file implementation

--- BNAlgo.hpp
+++ BNAlgo.hpp
@@ -5,20 +5,32 @@
 #ifndef ASSIGNMENT1_BNALGO_HPP
 #define ASSIGNMENT1_BNALGO_HPP
 #include <vector>
+#include <opencv2/opencv.hpp>
+
+struct LinePair {
+    cv::Point2f P_source, Q_source;
+    cv::Point2f P_dest, Q_dest;
+};
 
 class BNAlgo {
 private:
-    std::vector<std::vector<int>> img1;
-    int img1Weight = 50;
+    cv::Mat source;
+    cv::Mat destination;
+    std::vector<LinePair> lines;
 
-    std::vector<std::vector<int>> img2;
-    int img2Weight = 50;
+    float a = 1.0f;
+    float b = 2.0f;
+    float c = 0.5f;
 
-    std::vector<std::vector<int>> output;
+    cv::Point2f calculatePerpendicular(const cv::Point2f& vector);
+    float calculateDistance(const cv::Point2f& p1, const cv::Point2f& p2);
 
 public:
+    BNAlgo(const cv::Mat& src, const cv::Mat& dst);
 
+    void addLinePair(cv::Point2f P_src, cv::Point2f Q_src, cv::Point2f P_dst, cv::Point2f Q_dst);
 
+    cv::Mat warpImage();
 };
 
 
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Conner on 2026-03-24.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main() {
+
+}
