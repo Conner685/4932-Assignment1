a96bc794ae80371956113f5f5b389ec685508755
Conner685 <cponton@my.bcit.ca>
2026-03-24 13:07:19 -0700

    simplified cross disolve created in main

--- main.cpp
+++ main.cpp
@@ -6,5 +6,28 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
+    for (int i = 1; i <= 8; i++) {
+        std::string w0_filename = "W0.t" + std::to_string(i) + ".jpg";
+        std::string w1_filename = "W1.t" + std::to_string(i) + ".jpg";
 
+        cv::Mat W0 = cv::imread(w0_filename);
+        cv::Mat W1 = cv::imread(w0_filename);
+
+        if (W0.empty() || W1.empty()) {
+            std::cerr << "file not found" << std::endl;
+            return -1;
+        }
+
+        double alpha = static_cast<double>(i) / 8.0;
+        double beta = 1.0 - alpha;
+
+        cv::Mat blendedImg;
+        cv::addWeighted(W0, beta, W1, alpha, 0.0, blendedImg);
+
+        std::string output_filename = "Blended_t" + std::to_string(i) + ".jpg";
+        cv::imwrite(output_filename, blendedImg);
+        std::cout << "img " << i << " created";
+    }
+
+    return 0;
 }
